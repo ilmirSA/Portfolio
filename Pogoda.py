16efbfca8728442f5609c96e209f2f9f0f934b4b
ilmirSA <71828619+ilmirSA@users.noreply.github.com>
2020-12-26 01:21:05 +0300

    Update Pogoda.py

--- Pogoda.py
+++ Pogoda.py
@@ -1,9 +1,15 @@
 import requests
 
-cities = ["London", "Sheremetyevo", "Cherepovets"]
-pattern = 'http://wttr.in/{}?n'
-for insert in cities:
-    url = pattern.format(insert)
+pattern = 'http://wttr.in/{}'
+
+dict = {
+    "Лондон?nTqmM&lang=ru": "",
+    "Шереметьево?nTqm&lang=ru": "",
+    "Череповец?nTqm&lang=ru": "",
+}
+
+for city in dict:
+    url = pattern.format(city)
     response = requests.get(url)
     response.raise_for_status()
     print(response.text)
