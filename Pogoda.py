14d7dd2e62e9419917a5c1745d57bb804c669845
ilmirSA <71828619+ilmirSA@users.noreply.github.com>
2020-12-27 23:56:40 +0300

    Update Pogoda.py

--- Pogoda.py
+++ Pogoda.py
@@ -1,18 +1,14 @@
 import requests
 
-url= 'http://wttr.in/{}'
+url = 'http://wttr.in/{}'
 
-key= {
-    "Лондон": "",
-    "Шереметьево": "",
-    "Череповец": "",
-}
-value={"?nTqmM":"",
-     "lang":"ru",}
+key = ["Лондон", "Шереметьево", "Череповец", ]
+
+value = {"nTqmM": "",
+         "lang": "ru", }
 
 for city in key:
     response = url.format(city)
-    response = requests.get(response,params=value)
+    response = requests.get(response, params=value)
     response.raise_for_status()
     print(response.text)
-
